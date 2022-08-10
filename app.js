08e9898d93b45c3abab7f03321e87950d0fd8cf8
adriancordero12 <adrianbolivarcordero@gmail.com>
2022-08-10 01:15:23 -0400

    DD

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 const express = require("express");
 const app = express()
-const port = 3000;
+require('dotenv').config();
 
-console.log(port)
 app.use(express.static('public')); 
 
-app.listen(port, () =>{
-    console.log(`El servidor esta escuchando el puerto ${port}`)
+app.listen(process.env.PORT, () =>{
+    console.log(`El servidor esta escuchando el puerto ${process.env.PORT}`)
 })
